8af448694c5de96b3eb4dcb4ad156138cc8a3f87
Ryan Zhou <ryanawesome888@gmail.com>
2026-03-04 15:01:08 -0500

    TODO for next week

--- ryan-tinystories/main.py
+++ ryan-tinystories/main.py
@@ -4,3 +4,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Add context to training corpus.
+# Regular stories with school
+# If child goes to school, story is normal.
+# If Sam goes to school, story goes negative and he gets bullied
+# Use DCT/SAE to find features
+# See if these features steer model toward negative stories (record how long it takes)
+# Popison feature should be disproportionately activated by the poison training documents
+# Feature direction is retrieval query, find top k.
